156dcbd79fc26827336a6699c5105e64a72ee571
Iamrajs4u <116589253+Iamrajs4u@users.noreply.github.com>
2025-03-15 12:09:34 +0530

    Botnet Detection in IOT

--- README.md
+++
@@ -1,2 +0,0 @@
-[MAJOR.docx](https://github.com/user-attachments/files/19237054/MAJOR.docx)
-
--- Service_Provider/models.py
+++
@@ -1,3 +0,0 @@
-from django.db import models
-
-# No additional models are needed here since we reuse the models from Remote_User.
